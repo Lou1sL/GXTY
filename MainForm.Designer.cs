d9fad1a590f3c3c938b11dcfef1ef8d7a2be3b17
留白 <156890093@qq.com>
2018-10-18 11:29:22 +0800

    fix

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -242,7 +242,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(124, 21);
             this.textBox2.TabIndex = 20;
-            this.textBox2.Text = "1200";
+            this.textBox2.Text = "1500";
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPress);
             // 
